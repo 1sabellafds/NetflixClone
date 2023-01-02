341f5a467533235dbc04c2b12b7e208f5b672513
1sabellafds <isabella2016ferreira@gmail.com>
2023-01-01 21:36:08 -0300

    firstcommit

---
+++ public/js/api.js
@@ -0,0 +1,22 @@
+let api_key = "c6c34489ed43b070d9decbc7db8c8bb6"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,23 @@
+// importando
+
+const express = require("express"); // criar o servidor local
+const path = require("path") //importanto os caminhos - biblioteca que ja vem no node
+const port = 3333
+
+let initialPath = path.join(__dirname, "public") // pega o caminho atual e concatena com a pasta escolhida
+
+let app = express() //Creates an Express application
+app.use(express.static(initialPath)); // Criou o servidor para o initialpath da pasta public
+
+//ROTAS
+
+// Nessa rota está passando para o servidor que sempre que ele for chamado e não tiver nada depois da barra ("/")
+// manda uma resposta enviando o arquivo initialfile que pertence a public
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(initialPath, "index.html"))
+});
+
+// iniciar o servidor na porta e pegando o log dele.
+app.listen(port, () =>{
+    console.log(`Server ta funcionando caraii!! Na porta ${port}`)
+})
